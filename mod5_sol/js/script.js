97a726e94bf774730057932c6f716fcafa0ab9d9
Ali Hasan <ali1salem1hasan@gmail.com>
2020-09-01 20:56:46 +0300

    modefying files ... job done

--- mod5_sol/js/script.js
+++ mod5_sol/js/script.js
@@ -42,6 +42,7 @@ var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
+
   return string;
 };
 
@@ -83,8 +84,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  buildAndShowHomeHTML);// ***** <---- TODO: STEP 1: Substitute [...] ******
+   // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -98,12 +99,18 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
 
+      var randomCategoryName=chooseRandomCategory(categories).short_name;
+   
+      
+   
+     
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-
-
+     
+         randomCategoryName="\'" + randomCategoryName+"\'";
+      
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -116,12 +123,13 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-
+        homeHtml= insertProperty(homeHtml,"randomCategoryShortName",randomCategoryName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+       insertHtml("#main-content",homeHtml);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
@@ -197,6 +205,7 @@ function buildCategoriesViewHtml(categories,
     // Insert category values
     var html = categoryHtml;
     var name = "" + categories[i].name;
+
     var short_name = categories[i].short_name;
     html =
       insertProperty(html, "name", name);
